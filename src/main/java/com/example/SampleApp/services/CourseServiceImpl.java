1a2c34a1aa6fa6cf2327ceed7053c1c7175aaed1
adithis197 <50405138+adithis197@users.noreply.github.com>
2021-06-30 21:09:11 +0530

    Update CourseServiceImpl.java

--- src/main/java/com/example/SampleApp/services/CourseServiceImpl.java
+++ src/main/java/com/example/SampleApp/services/CourseServiceImpl.java
@@ -3,7 +3,7 @@ import com.example.SampleApp.entities.Chat;
 import com.example.SampleApp.entities.Courses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory. annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
